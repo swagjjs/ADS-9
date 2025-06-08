d55455a782f1a4a40e5ff46e3a1aa369d8eaa176
swagjjs <liazemcova00@gmail.com>
2025-06-08 22:13:32 +0300

    Update tree.h

--- include/tree.h
+++ include/tree.h
@@ -1,5 +1,31 @@
 // Copyright 2022 NNTU-CS
-#ifndef INCLUDE_TREE_H_
-#define INCLUDE_TREE_H_
+#ifndef TREE_H
+#define TREE_H
 
-#endif  // INCLUDE_TREE_H_
+#include <vector>
+#include <memory>
+
+class PMTree {
+ private:
+    struct Node {
+        char value;
+        std::vector<std::shared_ptr<Node>> children;
+        Node(char v) : value(v) {}
+    };
+
+    std::shared_ptr<Node> root;
+    std::vector<char> elements;
+    size_t factorial(size_t n) const;
+
+ public:
+    explicit PMTree(const std::vector<char>& in);
+    std::vector<std::vector<char>> getAllPerms() const;
+    std::vector<char> getPermByIndex(size_t index) const;
+    std::vector<char> getPermByTraversal(size_t index) const;
+};
+
+std::vector<std::vector<char>> getAllPerms(const PMTree& tree);
+std::vector<char> getPerm1(PMTree& tree, int num);
+std::vector<char> getPerm2(PMTree& tree, int num);
+
+#endif  // TREE_H
